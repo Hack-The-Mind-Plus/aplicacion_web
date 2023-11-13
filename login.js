9ae13a23c0498b189b10e118170c1f9a041c276b
AlanAcosta098 <95245525+AlanAcosta098@users.noreply.github.com>
2023-11-12 23:52:13 -0600

    Update login.js
    cambio de .js para optimizar para celular y modificacion de animaciones

--- login.js
+++ login.js
@@ -1,33 +1,68 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var $loginMsg = $('.loginMsg'),
-    $login = $('.login'),
-    $signupMsg = $('.signupMsg'),
-    $signup = $('.signup'),
-    $frontbox = $('.frontbox');
+    var $loginMsg = $('.loginMsg'),
+        $login = $('.login'),
+        $signupMsg = $('.signupMsg'),
+        $signup = $('.signup'),
+        $frontbox = $('.frontbox'),
+        $mainHeader = $('.main-header');
 
-  $('#switch1').on('click', function() {
-    $loginMsg.toggleClass("visibility");
-    $frontbox.addClass("moving");
-    $signupMsg.toggleClass("visibility");
+ // Función para intentar iniciar sesión
+    window.attemptLogin = function() {
+        // Lógica de inicio de sesión...
+        // En caso de error, redirigir a la página de mensajes
+        showMessage("error404");
+        window.location.href = "messages.html#error404";
+    };
 
-    $signup.toggleClass('hide');
-    $login.toggleClass('hide');
-  });
+    // Función para intentar registrarse
+    window.attemptSignup = function() {
+        // Lógica de registro...
+        // En caso de error, redirigir a la página de mensajes
+        showMessage("error401");
+        window.location.href = "messages.html#error401";
+    };
 
-  $('#switch2').on('click', function() {
-    $loginMsg.toggleClass("visibility");
-    $frontbox.removeClass("moving");
-    $signupMsg.toggleClass("visibility");
+    $('#switch1').on('click', function() {
+        $loginMsg.toggleClass("visibility");
+        $frontbox.addClass("moving");
+        $signupMsg.toggleClass("visibility");
 
-    $signup.toggleClass('hide');
-    $login.toggleClass('hide');
-  });
+        $signup.removeClass('hide');
+        $login.addClass('hide');
 
-  setTimeout(function(){
-    $('#switch1').click();
-  }, 1000);
+        $frontbox.removeClass('hide');
 
-  setTimeout(function(){
-    $('#switch2').click();
-  }, 3000);
+        // Desplazar la página hacia el frontbox de registro
+        $frontbox[0].scrollIntoView({ behavior: 'smooth', block: 'center' });
+
+        // Ocultar el encabezado al hacer clic en el botón
+        $mainHeader.addClass('hide-header');
+    });
+
+    $('#switch2').on('click', function() {
+        $loginMsg.toggleClass("visibility");
+        $frontbox.removeClass("moving");
+        $signupMsg.toggleClass("visibility");
+
+        $signup.addClass('hide');
+        $login.removeClass('hide');
+
+        $frontbox.removeClass('hide');
+
+        // Desplazar la página hacia el frontbox de inicio de sesión
+        $frontbox[0].scrollIntoView({ behavior: 'smooth', block: 'center' });
+
+        // Ocultar el encabezado al hacer clic en el botón
+        $mainHeader.addClass('hide-header');
+    });
+
+    // Ocultar el frontbox al cargar la página
+    $frontbox.addClass('hide');
+
+    // Mostrar el encabezado al llegar al principio de la página
+    window.addEventListener('scroll', function() {
+        if (window.scrollY === 0) {
+            $mainHeader.removeClass('hide-header');
+        }
+    });
 });
